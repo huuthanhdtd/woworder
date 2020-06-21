f1cfdf9cb4ba44a8cc5a2a83963e5ab738d318a4
huuthanhdtd <huuthanhdtd1@gmail.com>
2020-06-21 23:30:34 +0900

    fix brand logo

--- pages/design.vue
+++ pages/design.vue
@@ -4,11 +4,11 @@
       <div class="flex flex-wrap justify-between mb-8">
         <div class="lg:w-6/12 lg:text-left text-center p-4 sm:p-0">
           <h1 class="text-3xl xl:text-4xl text-light-onSurfacePrimary dark:text-dark-onSurfacePrimary font-medium leading-normal mb-6 lg:pt-4">
-            Nuxt<span class="text-primary-base">JS</span> Design
+            Nhận diện thương hiệu Wow<span class="text-primary-base">Order</span>
           </h1>
           <h3 class="xl:text-lg text-light-onSurfaceSecondary dark:text-dark-onSurfaceSecondary font-medium leading-relaxed mb-6">
-            NuxtJS is an MIT licensed open source project and completely free to use.<br>
-            You can freely use our logos as long as you mention NuxtJS and link to nuxtjs.org.
+            Bộ nhận diện thương hiệu của WowOrder.<br>
+            Bản quyền thuộc về woworder.org.
           </h3>
         </div>
         <DesignIllustration class="w-2/3 mx-auto lg:mx-0 lg:w-5/12 lg:-mt-8 text-light-elevatedSurface dark:text-dark-elevatedSurface"/>
@@ -20,26 +20,26 @@
               Light theme
             </h2>
             <figure class="rounded hover:bg-gray-200">
-              <img src="/logos/nuxt.svg" alt="nuxt-logo">
-              <figcaption><a href="/logos/nuxt.svg" target="_blank" rel="noopener">nuxt.svg</a> / <a href="/logos/nuxt-square.svg" target="_blank" rel="noopener">nuxt-square.svg</a></figcaption>
+              <img src="/logos/tctt.svg" alt="nuxt-logo">
+              <figcaption><a href="/logos/tctt.svg" target="_blank" rel="noopener">tctt.svg</a> / <a href="/logos/tctt-square.svg" target="_blank" rel="noopener">tctt-square.svg</a></figcaption>
             </figure>
             <figure class="rounded hover:bg-gray-200 mt-6">
-              <img src="/logos/nuxt-icon.png" srcset="/logos/nuxt-icon@2x.png 2x" alt="nuxt-logo">
-              <figcaption><a href="/logos/nuxt-icon.png" target="_blank" rel="noopener">nuxt-icon.png</a></figcaption>
+              <img src="/logos/tctt-icon.png" srcset="/logos/tctt-icon@2x.png 2x" alt="tctt-logo">
+              <figcaption><a href="/logos/tctt-icon.png" target="_blank" rel="noopener">tctt-icon.png</a></figcaption>
               <span class="dimensions">512 x 512</span>
             </figure>
             <figure class="rounded hover:bg-gray-200 mt-6">
-              <img src="/logos/nuxt-emoji.png" srcset="/logos/nuxt-emoji@2x.png 2x" alt="nuxt-logo">
-              <figcaption><a href="/logos/nuxt-emoji.png" target="_blank" rel="noopener">nuxt-emoji.png</a></figcaption>
+              <img src="/logos/tctt-emoji.png" srcset="/logos/tctt-emoji@2x.png 2x" alt="tctt-logo">
+              <figcaption><a href="/logos/tctt-emoji.png" target="_blank" rel="noopener">tctt-emoji.png</a></figcaption>
               <span class="dimensions">128 x 128</span>
             </figure>
             <figure class="rounded hover:bg-gray-200 mt-6">
-              <img src="/logos/nuxtjs-typo.svg" alt="nuxt-logo">
-              <figcaption><a href="/logos/nuxtjs-typo.svg" target="_blank" rel="noopener">nuxtjs-typo.svg</a></figcaption>
+              <img src="/logos/tctt-typo.svg" alt="tctt-logo">
+              <figcaption><a href="/logos/tctt-typo.svg" target="_blank" rel="noopener">tctt-typo.svg</a></figcaption>
             </figure>
             <figure class="rounded hover:bg-gray-200 mt-6">
-              <img src="/logos/built-with-nuxt.svg" alt="built-with-nuxt-logo">
-              <figcaption><a href="/logos/built-with-nuxt.svg" target="_blank" rel="noopener">built-with-nuxt.svg</a></figcaption>
+              <img src="/logos/built-with-tctt.svg" alt="built-with-tctt-logo">
+              <figcaption><a href="/logos/built-with-tctt.svg" target="_blank" rel="noopener">built-with-tctt.svg</a></figcaption>
             </figure>
           </section>
         </div>
@@ -49,32 +49,32 @@
               Dark theme
             </h2>
             <figure class="rounded hover:bg-gray-800">
-              <img src="/logos/nuxt-white.svg" alt="nuxt-logo">
-              <figcaption><a href="/logos/nuxt-white.svg" target="_blank" rel="noopener">nuxt-white.svg</a> / <a href="/logos/nuxt-square-white.svg" target="_blank" rel="noopener">nuxt-square-white.svg</a></figcaption>
+              <img src="/logos/tctt-white.svg" alt="tctt-logo">
+              <figcaption><a href="/logos/tctt-white.svg" target="_blank" rel="noopener">tctt-white.svg</a> / <a href="/logos/tctt-square-white.svg" target="_blank" rel="noopener">tctt-square-white.svg</a></figcaption>
             </figure>
             <figure class="rounded hover:bg-gray-800 mt-6">
-              <img src="/logos/nuxt-icon-white.png" srcset="/logos/nuxt-icon-white@2x.png 2x" alt="nuxt-logo">
-              <figcaption><a href="/logos/nuxt-icon-white.png" target="_blank" rel="noopener">nuxt-icon-white.png</a></figcaption>
+              <img src="/logos/tctt-icon-white.png" srcset="/logos/tctt-icon-white@2x.png 2x" alt="tctt-logo">
+              <figcaption><a href="/logos/tctt-icon-white.png" target="_blank" rel="noopener">tctt-icon-white.png</a></figcaption>
               <span class="dimensions">512 x 512</span>
             </figure>
             <figure class="rounded hover:bg-gray-800 mt-6">
-              <img src="/logos/nuxt-emoji-white.png" srcset="/logos/nuxt-emoji-white@2x.png 2x" alt="nuxt-logo">
-              <figcaption><a href="/logos/nuxt-emoji-white.png" target="_blank" rel="noopener">nuxt-emoji-white.png</a></figcaption>
+              <img src="/logos/tctt-emoji-white.png" srcset="/logos/tctt-emoji-white@2x.png 2x" alt="tctt-logo">
+              <figcaption><a href="/logos/tctt-emoji-white.png" target="_blank" rel="noopener">tctt-emoji-white.png</a></figcaption>
               <span class="dimensions">128 x 128</span>
             </figure>
             <figure class="rounded hover:bg-gray-800 mt-6">
-              <img src="/logos/nuxtjs-typo-white.svg" alt="nuxt-logo">
-              <figcaption><a href="/logos/nuxtjs-typo-white.svg" target="_blank" rel="noopener">nuxtjs-typo-white.svg</a></figcaption>
+              <img src="/logos/tctt-typo-white.svg" alt="tctt-logo">
+              <figcaption><a href="/logos/tctt-typo-white.svg" target="_blank" rel="noopener">tctt-typo-white.svg</a></figcaption>
             </figure>
             <figure class="rounded hover:bg-gray-800 mt-6">
-              <img src="/logos/built-with-nuxt-white.svg" alt="built-with-nuxt-logo">
-              <figcaption><a href="/logos/built-with-nuxt-white.svg" target="_blank" rel="noopener">built-with-nuxt-white.svg</a></figcaption>
+              <img src="/logos/built-with-tctt-white.svg" alt="built-with-tctt-logo">
+              <figcaption><a href="/logos/built-with-tctt-white.svg" target="_blank" rel="noopener">built-with-tctt-white.svg</a></figcaption>
             </figure>
           </section>
         </div>
       </div>
       <p class="text-center mt-8 text-light-onSurfacePrimary dark:text-dark-onSurfacePrimary transition-colors duration-300 ease-linear">
-        You can also download our <a href="/favicon.ico" target="_blank" rel="noopener">favicon</a> or our  <a href="/logos/nuxt-logos.sketch" target="_blank" rel="noopener">Sketch file</a>.
+        You can also download our <a href="/favicon.ico" target="_blank" rel="noopener">favicon</a> or our  <a href="/logos/tctt-logos.sketch" target="_blank" rel="noopener">Sketch file</a>.
       </p>
     </div>
   </div>
@@ -89,8 +89,8 @@ export default {
     DesignIllustration
   },
   head () {
-    const title = 'Design Resources - NuxtJS'
-    const description = 'Download NuxtJS design resources (SVG, icons, emoji and favicon).'
+    const title = 'Nhận diện thương hiệu - WowOrder'
+    const description = 'Bộ nhận diện thương hiệu của WowOrder. Bản quyền thuộc về woworder.org.'
 
     return {
       title,
